994e5bced2c253669051a5d8af5083ab988165bd
TheLKL321 <lkl.zarebski@gmail.com>
2018-01-05 13:06:31 +0100

    add inDegrees

--- topol.ml
+++ topol.ml
@@ -11,8 +11,8 @@ exception Cykliczne;;
 type 'a graf = ('a * 'a list) list
 
 (*  Zwraca mapę list node'ów do których dany node ma skierowaną krawędź, 
-    kluczowaną node'ami. W żadnej z takich list nie występują duplikaty, oraz 
-    każdy node grafu znajduje się w uzyskanej mapie *)
+    kluczowaną node'ami. W żadnej z takich list nie występują duplikaty, lecz 
+    niekoniecznie każdy element listy znajduje się w uzyskanej mapie jako node*)
 let kartograf g =
   let mapa = ref empty
   in let f (node, lis) = 
@@ -31,14 +31,28 @@ let kartograf g =
             tempMapa := add h h !tempMapa;
             loop t
         in loop l;
-        (*  dla każdego unikatowego sąsiada dodaj go jako node jeśli jeszcze go nie ma *)
         fold (fun k a -> k :: a) !tempMapa []
     in map uniq !mapa
 
+(*  Tworzy mapę kluczowaną node'ami, zawierającą liczbę krawędzi skierowanych
+    do danego node'a *)
+let inDegrees m =
+  let indegree = ref empty
+  in let f k lis = 
+    indegree := add k 0 !indegree;
+    let filler x = 
+      try
+        indegree := add x (find x !indegree + 1) !indegree;
+      with
+        | Not_found -> indegree := add x 1 !indegree;
+        | _ -> failwith "Unknown"
+    in List.iter filler lis;
+  in iter f m;
+  indegree
 
 (*  Dla danego grafu[(a_1,[a_11;...;a_1n]); ...; (a_m,[a_m1;...;a_mk])] zwraca 
     graf w ktorym kazdy z elementow a_i oraz a_ij wystepuje dokladnie raz i 
     ktory jest uporzadkowany w taki sposob, ze kazdy element a_i jest przed
     kazdym z elementow a_i1 ... a_il *)
-let topol g = 42
+let topol g = 
 ;;
